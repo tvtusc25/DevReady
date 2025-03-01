37304f42d01b2c586e98c33bc5ffbf8bbe5ada87
Ryan Mogauro <89362786+ryanmogauro@users.noreply.github.com>
2025-03-01 14:28:20 -0500

    removed setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-setup(
-    name="devready",
-    packages=find_packages(),
-    include_package_data=True,
-) 
+# setup(
+#     name="devready",
+#     packages=find_packages(),
+#     include_package_data=True,
+# )
